9a1a82073934dc018af6a3abc3b35d10c1c9b1f5
gig121212 <sunyeongbang98@gmail.com>
2023-05-14 07:43:31 +0900

    -update news views.py

--- news/core/views.py
+++ news/core/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from listings.models import Listing
 
 
+
+
+
+#making trend list function
+# using order_by()method
 def index(request):
     listings = Listing.objects.all()
     popular_lists = Listing.objects.all().order_by('-viewCount')
@@ -11,14 +16,7 @@ def index(request):
     }
     return render(request, 'core/index.html', context)
 
-#making trend list function
-# using order_by()method
-# def popular_list(request):
-#     popular_lists = Listing.objects.all().order_by('-viewCount')
-#     context = {
-#         'popular_lists':popular_lists,
-#     }
-#     return render(request,'core/index.html',context)
+
 
 def contact(request):
     return render(request, 'core/contact.html')
